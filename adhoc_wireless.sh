aed09997cae807d67f7a0567f1a9350202a9b065
Paul Ruvolo <paullundyruvolo@gmail.com>
2014-09-13 10:50:31 -0400

    added wireless config scripts

---
+++ adhoc_wireless.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+echo "Starting up adhoc wireless"
+echo "Make sure to use sudo when executing"
+echo ""
+
+stop network-manager
+ifconfig wlan0 down
+sleep 2
+iwconfig wlan0 mode ad-hoc
+iwconfig wlan0 key aaaaa11111
+iwconfig wlan0 essid $1
+ifconfig wlan0 10.0.0.100 netmask 255.255.255.0 up
+
+ping 10.0.0.200
---
+++ normal_wireless.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "Starting up normal wireless"
+echo "Make sure to use sudo when executing"
+
+start network-manager
