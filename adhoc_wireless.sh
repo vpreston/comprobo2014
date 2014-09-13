2b023f5b40109a6de7426e330077869f34eaf673
Paul Ruvolo <paullundyruvolo@gmail.com>
2014-09-13 13:08:51 -0400

    updated to force channel 1

--- adhoc_wireless.sh
+++ adhoc_wireless.sh
@@ -9,6 +9,7 @@ stop network-manager
 ifconfig wlan0 down
 sleep 2
 iwconfig wlan0 mode ad-hoc
+iwconfig wlan0 channel 1
 iwconfig wlan0 key aaaaa11111
 iwconfig wlan0 essid $1
 ifconfig wlan0 10.0.0.100 netmask 255.255.255.0 up
