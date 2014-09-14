125a35db8a271ae451c56db9973fe27c73ddd4fe
paulruvolo <paullundyruvolo@gmail.com>
2014-09-14 18:37:47 -0400

    updated adhoc wireless

--- adhoc_wireless.sh
+++ adhoc_wireless.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+COUNT=`expr $1 - 100`
+echo $COUNT
 
 echo "Starting up adhoc wireless"
 echo "Make sure to use sudo when executing"
@@ -11,7 +13,7 @@ sleep 2
 iwconfig wlan0 mode ad-hoc
 iwconfig wlan0 channel 1
 iwconfig wlan0 key aaaaa11111
-iwconfig wlan0 essid $1
-ifconfig wlan0 10.0.0.100 netmask 255.255.255.0 up
+iwconfig wlan0 essid RPi
+ifconfig wlan0 10.0.0.$COUNT netmask 255.255.255.0 up
 
-ping 10.0.0.200
+ping 10.0.0.$1
