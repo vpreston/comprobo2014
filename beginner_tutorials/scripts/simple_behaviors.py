3c336a8627d21fc913261024b0a1fd6aa3fadd47
Victoria Preston <victoria@snapcracklepop.(none)>
2014-09-17 18:13:06 -0400

    Full Warm Up Completed

--- beginner_tutorials/scripts/simple_behaviors.py
+++ beginner_tutorials/scripts/simple_behaviors.py
@@ -20,6 +20,8 @@ lead_left_avg = 0
 lead_right_avg = 0
 trailing_left_avg = 0
 trailing_right_avg = 0
+avoid_force = 0
+avoid_angle = 0
 
 def front_scan_recieved(msg):
     """Process data from laser scanner, msg is of type sensor_msgs/LaserScan"""
@@ -39,12 +41,13 @@ def wall_approach():
     """The publisher for a Neato Controller"""
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=10) #we're publishing to cmd_vel with a type Twist
     sub = rospy.Subscriber('scan', LaserScan, front_scan_recieved)
-    rospy.init_node('wall_approach', anonymous=True)
+    #rospy.init_node('wall_approach', anonymous=True)
     r = rospy.Rate(10) # 10hz
     global mean_distance
     while not rospy.is_shutdown():
         if mean_distance > 0.98 and mean_distance < 1.02:
             msg = Twist(Vector3(0.0,0.0,0.0), Vector3(0.0,0.0,0.0))
+            return mean_distance
         else:
             prop = 0.2*(mean_distance - 1.0)
             msg = Twist(Vector3(prop,0.0,0.0), Vector3(0.0,0.0,0.0))
@@ -75,7 +78,7 @@ def people_follow():
     """The publisher for a Neato Controller"""
     pub_people = rospy.Publisher('cmd_vel', Twist, queue_size=10) #we're publishing to cmd_vel with a type Twist
     sub_people = rospy.Subscriber('scan', LaserScan, people_scan_recieved)
-    rospy.init_node('people_follow', anonymous=True)
+    #rospy.init_node('people_follow', anonymous=True)
     r = rospy.Rate(10) # 10hz
     global people_distance
     global people_angle
@@ -139,7 +142,7 @@ def wall_follow():
     """The publisher for a Neato Controller"""
     pub_wall = rospy.Publisher('cmd_vel', Twist, queue_size=10) #we're publishing to cmd_vel with a type Twist
     sub_wall = rospy.Subscriber('scan', LaserScan, wall_scan_received)
-    rospy.init_node('wall_follow', anonymous=True)
+    #rospy.init_node('wall_follow', anonymous=True)
     r = rospy.Rate(10) # 10hz
     global behavior
     global lead_left_avg
@@ -177,6 +180,55 @@ def wall_follow():
         pub_wall.publish(msg_wall)
         r.sleep()
 
+def full_scan_recieved(msg_avoid):
+    """Process data from laser scanner, msg is of type sensor_msgs/LaserScan"""
+    #print "front scan received"
+    left_obstacles = {}
+    right_obstacles = {}
+    global avoid_force
+    global avoid_angle
+    for i in range(89):
+        #print msg_avoid.ranges[i]
+        if msg_avoid.ranges[i] > 0 and msg_avoid.ranges[i] < 3:
+            left_obstacles[msg_avoid.ranges[i]] = i
+        if msg_avoid.ranges[i+270] > 0 and msg_avoid.ranges[i+270] < 3:
+            right_obstacles[msg_avoid.ranges[i+270]] = i+270
+        if len(left_obstacles) > len(right_obstacles):
+            avoid_force = min(left_obstacles.keys())
+            avoid_angle = left_obstacles[avoid_force]
+        elif len(left_obstacles) < len(right_obstacles):
+            avoid_force = min(right_obstacles.keys())
+            avoid_angle = right_obstacles[avoid_force]
+        else:
+            avoid_force = 1.0
+            avoid_angle = 0
+
+def obstacle_avoid():
+    """The publisher for a Neato Controller"""
+    pub_avoid = rospy.Publisher('cmd_vel', Twist, queue_size=10) #we're publishing to cmd_vel with a type Twist
+    sub_avoid = rospy.Subscriber('scan', LaserScan, full_scan_recieved)
+    #rospy.init_node('obstacle_avoid', anonymous=True)
+    r = rospy.Rate(10) # 10hz
+    global avoid_force
+    global avoid_angle
+    while not rospy.is_shutdown():
+        print avoid_force
+        print avoid_angle
+        if avoid_force > 1.0:
+            print 'nope'
+            msg_avoid = Twist(Vector3(0.1,0.0,0.0), Vector3(0.0,0.0,0.0))
+        else:
+            print 'yikes'
+            mag_turn = 0.005*avoid_angle
+            if avoid_angle < 90:
+                msg_avoid = Twist(Vector3(0.1,0.0,0.0), Vector3(0.0,0.0,-mag_turn))
+            else:
+                msg_avoid = Twist(Vector3(0.1,0.0,0.0), Vector3(0.0,0.0,mag_turn))
+
+        #msg = Twist(Vector3(0.0, 0.0, 0.0), Vector3(0.0, 0.0, 2.0))
+        #msg = Twist(angular=Vector3(z-2.0))
+        pub_avoid.publish(msg_avoid)
+        r.sleep()
 
 def get_keycmds():
     """Return the next character typed on the keyboard"""
@@ -191,6 +243,11 @@ def get_keycmds():
 
         
 if __name__ == '__main__':
+    rospy.init_node('people_follow')
     try:
-        wall_follow()
+        # while mean_distance < 0.98 or mean_distance > 1.02:
+        #     wall_approach()
+        # wall_follow()
+        people_follow()
+
     except rospy.ROSInterruptException: pass
