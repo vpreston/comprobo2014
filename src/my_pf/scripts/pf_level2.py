8067d6a760706535de7bd6539217eb3d1f842f62
Paul Ruvolo <paullundyruvolo@gmail.com>
2014-09-30 23:05:31 -0400

    added a new helper function for particle resampling.  Also added some better header comments to some of the functions

--- src/my_pf/scripts/pf_level2.py
+++ src/my_pf/scripts/pf_level2.py
@@ -180,7 +180,13 @@ class ParticleFilter:
 		self.robot_pose = Pose()
 
 	def update_particles_with_odom(self, msg):
-		""" Implement a simple version of this (Level 1) or a more complex one (Level 2) """
+		""" Update the particles using the newly given odometry pose.
+			The function computes the value delta which is a tuple (x,y,theta)
+			that indicates the change in position and angle between the odometry
+			when the particles were last updated and the current odometry.
+
+			msg: this is not really needed to implement this, but is here just in case.
+		"""
 		new_odom_xy_theta = TransformHelpers.convert_pose_to_xy_and_theta(self.odom_pose.pose)
 		# compute the change in x,y,theta since our last update
 		if self.current_odom_xy_theta:
@@ -200,7 +206,11 @@ class ParticleFilter:
 		pass
 
 	def resample_particles(self):
-		""" Resample the particles according to the new particle weights """
+		""" Resample the particles according to the new particle weights.
+			The weights stored with each particle should define the probability that a particular
+			particle is selected in the resampling step.  You may want to make use of the given helper
+			function draw_random_sample.
+		"""
 		# make sure the distribution is normalized
 		self.normalize_particles()
 		# TODO: fill out the rest of the implementation
@@ -244,6 +254,22 @@ class ParticleFilter:
 		"""
 		bins = np.add.accumulate(probabilities)
 		return values[np.digitize(random_sample(size), bins)]
+
+	@staticmethod
+	def draw_random_sample(choices, probabilities, n):
+		""" Return a random sample of n elements from the set choices with the specified probabilities
+			choices: the values to sample from represented as a list
+			probabilities: the probability of selecting each element in choices represented as a list
+			n: the number of samples
+		"""
+		values = np.array(range(len(choices)))
+		probs = np.array(probabilities)
+		bins = np.add.accumulate(probs)
+		inds = values[np.digitize(random_sample(n), bins)]
+		samples = []
+		for i in inds:
+			samples.append(choices[int(i)])
+		return samples
 
 	def update_initial_pose(self, msg):
 		""" Callback function to handle re-initializing the particle filter based on a pose estimate.
