0ea0cbdc85d7532cafcb814294c23075f2962d72
vpreston <victorialynnpreston@gmail.com>
2014-10-15 22:16:16 -0400

    Finishing up CV tutorials

--- src/opencv_exercises/corner_detection.py
+++ src/opencv_exercises/corner_detection.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python
 
 #This is Harris Corner Detection
+#E = sum of (sindow * (shifted intensity - intensity)^2
+
+
+import cv2
+import numpy as np
+
+filename = 'lena.png'
+img = cv2.imread(filename)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+gray = np.float32(gray)
+dst = cv2.cornerHarris(gray,2,3,0.04)
+
+#result is dilated for marking the corners, not important
+dst = cv2.dilate(dst,None)
+
+# Threshold for an optimal value, it may vary depending on the image.
+img[dst>0.1*dst.max()]=[0,0,255]
+
+cv2.imshow('dst',img)
+if cv2.waitKey(0) & 0xff == 27:
+    cv2.destroyAllWindows()
